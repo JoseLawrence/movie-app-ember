b76c2d5a56b0494a57685fffcfef75cb1172b51a
Jose Lawrence <jose.mca2011@gmail.com>
2017-06-13 00:39:26 +0100

    Fix mixed spaces and tabs lint error

--- app/movie/model.js
+++ app/movie/model.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
- 	title: DS.attr('string'),
- 	icon: DS.attr('string'),
- 	genre: DS.attr('string'),
- 	description: DS.attr('string')
+	title: DS.attr('string'),
+	icon: DS.attr('string'),
+	genre: DS.attr('string'),
+	description: DS.attr('string')
 });
