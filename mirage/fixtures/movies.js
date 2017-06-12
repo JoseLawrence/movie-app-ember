37968b57cd328d2a63c68c41007efd8d05e9a857
Jose Lawrence <jose.mca2011@gmail.com>
2017-06-12 05:23:04 +0100

    First commit - add all files

---
+++ mirage/fixtures/movies.js
@@ -0,0 +1,44 @@
+export default [  
+    {
+		"id": "1",
+		"icon": "mov1.jpg",
+		"title": "The Mummy",
+		"genre": "Action",
+		"description": "Thought safely entombed in a tomb deep beneath the unforgiving desert, an ancient princess whose destiny was unjustly taken from her is awakened in our current day, bringing with her malevolence grown over millennia and terrors that defy human comprehension"
+	}, 
+	{
+		"id": "2",
+		"icon": "mov2.jpg",
+		"title": "Wonder Woman",
+		"genre": "Fantacy/Action",
+		"description": "Before she was Wonder Woman, she was Diana, princess of the Amazons, trained to be an unconquerable warrior. Raised on a sheltered island paradise, when an American pilot crashes on their shores and tells of a massive conflict raging in the outside world, Diana leaves her home, convinced she can stop the threat. Fighting alongside man in a war to end all wars, Diana will discover her full powers…and her true destiny."
+	}, 
+	{
+		"id": "3",
+		"icon": "mov3.jpg",
+		"title": "Pirates of the Caribbean",
+		"genre": "Action",
+		"description": "Johnny Depp returns to the big screen as the iconic, swashbuckling anti-hero Jack Sparrow in the all-new “Pirates of the Caribbean: Salazar's Revenge.” The rip-roaring adventure finds down-on-his-luck Captain"
+	}, 
+	{
+		"id": "4",
+		"icon": "mov4.jpg",
+		"title": "My Cousin Rachel",
+		"genre": "Romance",
+		"description": "A dark romance, MY COUSIN RACHEL tells the story of a young Englishman who plots revenge against his mysterious, beautiful cousin, believing that she murdered his guardian. But his feelings become complicated as he finds himself falling under the beguiling spell of her charms."
+	}, 
+	{
+		"id": "5",
+		"icon": "mov5.jpg",
+		"title": "Spider-Man: Homecoming",
+		"genre": "Fantacy",
+		"description": "A young Peter Parker/Spider-Man (Tom Holland), who made his sensational debut in Captain America: Civil War, begins to navigate his newfound identity as the web-slinging super hero in Spider-Man: Homecoming. Thrilled by his experience with the Avengers, Peter returns home, where he lives with his Aunt May, under the watchful eye of his new mentor Tony Stark"
+	}, 
+	{
+		"id": "6",
+		"icon": "mov6.jpg",
+		"title": "Gifted",
+		"genre": "Drama",
+		"description": "Frank Adler (Chris Evans) is a single man raising a child prodigy - his spirited young niece Mary (Mckenna Grace) - in a coastal town in Florida. Frank's plans for a normal school life for Mary are foiled when the seven-year-old’s mathematical abilities come to the attention of Frank’s formidable mother Evelyn (Lindsay Duncan) whose plans for her granddaughter threaten to separate Frank and Mary"
+	}, 
+];
